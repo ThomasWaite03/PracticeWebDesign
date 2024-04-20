5d0d36526bcb4b351271f86bdf5b7b78010e0b87
ThomasWaite03 <thomas.waite03@gmail.com>
2024-04-20 14:46:24 -0600

    Removed Utah as an option for locations on the main page.

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-var heroSectionLocations = ['Utah', 'Salt Lake City', 'Provo', 'Ogden', 'Logan', 'Cedar City', 'St. George']
+var heroSectionLocations = ['Salt Lake City', 'Provo', 'Ogden', 'Logan', 'Cedar City', 'St. George']
 $( "#hero-text-h1" ).on( "mouseleave", function() {
   $('#hero-text-h1').fadeOut('fast', function() {
     let currentIdx = heroSectionLocations.indexOf($(this).text());
